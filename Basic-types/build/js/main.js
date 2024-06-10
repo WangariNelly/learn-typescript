703897df84f4e75f6a181fd5f4d6d85fa566e940
WangariNelly <nellywangari30@gmail.com>
2024-06-10 13:01:22 +0300

    Intellisense and ts inference

---
+++ Basic-types/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+/* using intellisense, inference
+*/
+let myName = "Dave";
+let isEarth;
+let age;
+let localMap;
+let postId; //union type
+myName = '43';
+isEarth = true;
+const sum = (a, b) => {
+    return a + b;
+};
+postId = 'jycdecv76';
+let re = /\w+/g;
---
+++ Basic-types/src/main.ts
@@ -0,0 +1,20 @@
+/* using intellisense, inference
+*/
+let myName: string = "Dave"; 
+let isEarth: boolean;
+let age: number;
+let localMap: any;
+let postId: string | number; //union type
+
+myName = '43';
+isEarth = true;
+
+
+const sum = (a: number,b: number) => {
+  return  a + b;
+}
+
+postId = 'jycdecv76';
+ 
+let re: RegExp = /\w+/g;
+
