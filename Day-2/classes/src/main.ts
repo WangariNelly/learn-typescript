700f1808bd155f805f94e3391f1b65ee6a01ff02
WangariNelly <nellywangari30@gmail.com>
2024-06-13 11:21:06 +0300

    Classes

--- Day-2/classes/src/main.ts
+++ Day-2/classes/src/main.ts
@@ -14,7 +14,7 @@ class User{
 
 const user = new User ('Nelly',28,'Eng');
 
-class userDetails  extends User{
+class userDetails  extends User {
   constructor(
     public address: string,
     name: string,
@@ -22,7 +22,57 @@ class userDetails  extends User{
     lang: string,
   ) {
     super(name, age, lang)
-    this.address = address,
-
+    this.address = address
   }
 }
+
+//interface
+interface Musician {
+  name: string,
+  instrument: string,
+  play(action: string): string,
+}
+
+class Guitarist implements Musician {
+  name: string
+  instrument: string
+
+  constructor( name: string, instrument: string){
+    this.name = name 
+    this.instrument = instrument
+  }
+  play(action: string){
+    return `${this.name} ${action} the ${this.instrument}`
+  }
+
+}
+const Page = new Guitarist('Jimmy', 'guitar');
+console.log(Page.play('strums'))
+
+
+//////////////////////////////////////////////////////////
+class Peeps {
+  static count: number = 0
+
+  static getCount(): number {
+    return Peeps.count
+  }
+  public id: number;
+  constructor( public name: string){
+    this.name = name
+    this.id = ++ Peeps.count
+  }
+}
+
+const john = new Peeps('John')
+const amy = new Peeps('Amy')
+const Nelly = new Peeps('Nelly')
+
+
+console.log(Peeps.count)
+console.log(amy.id)
+
+////////////////////////////////////////////
+// getters and setters 
+
+
